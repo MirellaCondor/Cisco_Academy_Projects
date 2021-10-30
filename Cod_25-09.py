8ed653c4f5a661861fceec50759c06c643212714
unknown <Mirella Condor>
2021-10-30 17:05:30 -0500

    primer commit

---
+++ Cod_25-09.py
@@ -0,0 +1,13 @@
+print("Ingrese el nombre del alumno")
+nom=input() ## captura lo ingresado
+print(nom)  ## muestra lo ingresado
+## SE EJECUTA con F5
+print("Ingrese la nota 1:")
+N1=int(input())
+## Es la transformación de datos(casting)
+## En este caso de texto a número
+print("Ingrese la nota 2:")
+N2=int(input())
+
+Pr=(N1+N2)/2
+print("El promedio es:",Pr)
