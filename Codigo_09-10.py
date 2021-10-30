a52fde6b86123f8d6e091c3a60aa838a34ae2881
unknown <Mirella Condor>
2021-10-30 17:10:53 -0500

    primer commit

---
+++ Codigo_09-10.py
@@ -0,0 +1,43 @@
+##print("Ingrese el número")
+##Num=int(input())
+##
+##if(Num>0):
+##    print("El número es positivo...!!!")
+##    print("Es la respuesta verdadera...")
+##
+##else:
+##    print("El número no es positivo...!!!")
+
+
+##print("Ingrese iun número")
+##N=int(input())
+##
+##if(N%2==0):
+##    print("El número es par")
+##else:
+##    print("el número no es par")
+
+print("Ingrese el número")
+dia=int(input())
+
+if(dia==1):
+    print("Domingo...!!!")
+else:
+    if(dia==2):
+        print("Lunes")
+    else:
+        if(dia==3):
+            print("Martes")
+        else:
+            if(dia==4):
+                print("Miércoles")
+            else:
+                if(dia==5):
+                    print("Jueves")
+                else:
+                    if(dia==6):
+                        print("Viernes")
+                    else:
+                        print("Sábado")
+                  
+    
