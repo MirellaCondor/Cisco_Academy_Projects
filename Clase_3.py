fff1fbf56c3a692ebeebff8f9493b2beaf1f273d
unknown <Mirella Condor>
2021-11-11 12:04:38 -0500

    Primer commit

---
+++ Clase_3.py
@@ -0,0 +1,28 @@
+##print("Ingrese el valor de Y:")
+##y=int(input())
+##x=(8-4*y)/5
+##print("El valor de X es",x)
+
+##print("Ingrese 1er número:")
+##N1=int(input())
+##
+##print("Ingrese 2do número:")
+##N2=int(input())
+
+
+##print("Ingrese la diagonal menor:")
+##d=int(input())
+##print("Ingrese la diagonal mayor:")
+##D=int(input())
+##A=d*D/2
+##print("El área del rombo es:",A)
+
+##print(47/10)
+##print(47//10)
+##print(47%10)
+
+##print("Ingrese un número de 2 cifras")
+##Num=int(input())
+##U=Num%10
+##D=Num//10
+##print("La suma de los dígitos es:",U+D)
